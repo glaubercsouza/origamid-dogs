437df1e652cce8f89eeb1702f66d703fb8f39608
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2023-08-13 20:54:29 -0300

    feed

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import FeedPhotosItem from './FeedPhotosItem';
 import useFetch from '../../Hooks/useFetch';
-import Error from '../Helper/Error';
 import { PHOTOS_GET } from '../../Api';
+import Error from '../Helper/Error';
 import Loading from '../Helper/Loading';
-import styles from './FeedPhotos.module.css'
+import styles from './FeedPhotos.module.css';
 
 const FeedPhotos = () => {
   const { data, loading, error, request } = useFetch();
@@ -12,9 +12,10 @@ const FeedPhotos = () => {
   React.useEffect(() => {
     async function fetchPhotos() {
       const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
-      const { response, json } = await request(url, options);
+      const { json } = await request(url, options);
       console.log(json);
     }
+    fetchPhotos();
   }, [request]);
 
   if (error) return <Error error={error} />;
@@ -22,9 +23,9 @@ const FeedPhotos = () => {
   if (data)
     return (
       <ul className={`${styles.feed} animeLeft`}>
-        {data.map((photo) => {
-          <FeedPhotosItem key={photo.id} photo={photo} />;
-        })}
+        {data.map((photo) => (
+          <FeedPhotosItem key={photo.id} photo={photo} />
+        ))}
       </ul>
     );
   else return null;
