73fc3a3ae13bcb07d2c84264ecaa02225be67d12
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2023-08-26 21:10:24 -0300

    password lost

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -35,7 +35,7 @@ const LoginForm = () => {
         )}
         <Error error={error} />
       </form>
-      <Link className={styles.perdeu} to="/login/perder">
+      <Link className={styles.perdeu} to="/login/perdeu">
         Perdeu a Senha?
       </Link>
       <div className={styles.cadastro}>
