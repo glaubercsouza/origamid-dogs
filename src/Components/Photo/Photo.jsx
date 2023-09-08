0b7a3e4ac2fda31502d8f138cef556f75203a757
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2023-09-07 21:07:15 -0300

    final

--- src/Components/Photo/Photo.jsx
+++ src/Components/Photo/Photo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import useFetch from '../../Hooks/useFetch';
-import { PHOTO_GET_2 } from '../../Api';
+import { PHOTO_GET_2 } from '../../api';
 import Error from '../Helper/Error';
 import Loading from '../Helper/Loading';
 import PhotoContent from './PhotoContent';
