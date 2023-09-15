577ce0bcd61be0585a3f51906282722da9c91524
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2023-09-15 19:51:01 -0300

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,26 +14,26 @@ import NotFound from './Components/NotFound';
 
 const App = () => {
   return (
-    <div className='App'>
-      <BrowserRouter basename="/index.html">
+    <div className="App">
+      <BrowserRouter basename="../index.html">
         <UserStorage>
           <Header />
-          <main className='AppBody'>
-          <Routes>
-            <Route path="./" element={<Home />} />
-            <Route path="login/*" element={<Login />} />
-            <Route
-              path="conta/*"
-              element={
-                <ProtectedRoute>
-                  <User />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="foto/:id" element={<Photo />} />
-            <Route path="perfil/:user" element={<UserProfile />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <main className="AppBody">
+            <Routes>
+              <Route path="./" element={<Home />} />
+              <Route path="login/*" element={<Login />} />
+              <Route
+                path="conta/*"
+                element={
+                  <ProtectedRoute>
+                    <User />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="foto/:id" element={<Photo />} />
+              <Route path="perfil/:user" element={<UserProfile />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
           </main>
           <Footer />
         </UserStorage>
